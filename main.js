0fa083ee15665d9118a70ed20160a4d0891d33ef
karimy <karim.alrashdan14@gmail.com>
2022-09-10 08:17:58 -0600

    Created variables for comp items using querySelector method

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+var affirmationInput = document.querySelector(".wrapper-affirmation");
+var mantraInput = document.querySelector(".wrapper-mantra");
+var receiveMessageInput = document.querySelector(".receive-message");
+var randomMessage = document.querySelector(".message-goes-here");
+var yogiSymbol = document.querySelector(".meditate-image");

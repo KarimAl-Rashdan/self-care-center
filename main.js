00969bef8288bedf12409151153ac5440edffe47
karimy <karim.alrashdan14@gmail.com>
2022-09-11 04:15:03 -0600

    Fixed comp format issues

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-var affirmationInput = document.querySelector(".wrapper-affirmation");
+var formInput = document.querySelector(".wrapper-radio");
+// var affirmationInput = document.getElementById("affirmation");
 var mantraInput = document.querySelector(".wrapper-mantra");
-var receiveMessageInput = document.querySelector(".receive-message");
+var receiveMessageInput = document.getElementById("receive-message");
 var randomMessage = document.querySelector(".message-goes-here");
 var yogiSymbol = document.querySelector(".meditate-image");
+var affirmation = document.getElementById("affirmation");
+
+var fakeAffirmationData = ["Believe in yourself!", "You're doing great!", "Know your value!"]
+
+// affirmationInput.addEventListener('click', loadRandomMessage())
+receiveMessageInput.addEventListener('click', loadRandomMessage)
+
+// function chooseMessage() {
+//     receiveMessageInput
+// }
+
+ function loadRandomMessage() {
+   console.log("input");
+ console.log(formInput.value);
+   if(formInput.value === "affirmation") { //for loop
+     randomMessage.innerHTML = fakeAffirmationData[0];
+   }
+ }
+// make file for mantra data and affirmation data. assign variable to file
